caeda646cd7318fa9e80dd2a187be31a3ce02b9c
DevPhak <phakdi.w@outlook.com>
2021-02-26 10:31:12 +0700

    Commit

--- BMSDemoAPI/Startup.cs
+++ BMSDemoAPI/Startup.cs
@@ -26,9 +26,6 @@ namespace BMSDemoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            //Test Connection
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
